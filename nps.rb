7261ea229228f531cf78e087e335dd09d0117d28
Luciano Vieira <contato@lucianovieirapro.com>
2022-02-20 18:32:09 -0300

    att 20/02

--- nps.rb
+++ nps.rb
@@ -7,8 +7,11 @@ while em_aula do
     puts "#{nomeCliente} poderia participar da nossa pesquisa para melhoria do atendimento?"
     puts "Entre (1...10) qual nota indica ao seu atendimento?"
     nota = gets.to_f
-    puts "Obrigado pela sua nota #{nomeCliente}"
+    puts "Obrigado pela sua nota #{nomeCliente}!"
+    sleep (5)
+    system `clear`
     break if nota == -1
     notas << nota
 end
 puts "O seu NPS é de #{(notas.sum / notas.length)}"
+    
